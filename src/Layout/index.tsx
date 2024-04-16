d539f959acc0e44117542ef269b34587bf64cb36
Abdul Hamid <hamidkhattak469@gmail.com>
2024-04-16 23:00:22 +0500

    Sidebar UI completed

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,11 +1,14 @@
+import { ReactNode } from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 interface Props {
-    children: never;
+    children: ReactNode;
 }
 const Layout = ({ children }: Props) => {
   return (
     <div className="w-screen bg-slate-100 relative">
+      <Sidebar />
         <Navbar />
         <div className="w-full ml-[76px] mt-[70px] border-t border-gray-200 px-6 py-3 box-border flex flex-col">
             <span className="font-bold text-[#202224] text-[24px]">Dashboard</span>
