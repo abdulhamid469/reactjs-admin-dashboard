2b83c8f6c85a543cd93c107e952a4694b102dbcf
Abdul Hamid <hamidkhattak469@gmail.com>
2024-04-19 23:31:44 +0500

    Create ChartComponent and Design Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Layout from "./Layout";
+import SalesChat from "./components/SalesChat";
 import StartCards from "./components/StartCards";
 
 const App = () => {
   return (
     <Layout>
       <StartCards />
+      <SalesChat />
     </Layout>
   )
 }
